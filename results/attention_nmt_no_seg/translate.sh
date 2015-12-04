f30ee49ba4e4e23cead6352015bc0e01bebe41c5
QingxianLai <ql516@nyu.edu>
2015-12-04 15:50:44 -0500

    calculate BLUE scores

--- results/attention_nmt_no_seg/translate.sh
+++ results/attention_nmt_no_seg/translate.sh
@@ -5,25 +5,25 @@
 #PBS -N translate.seg.att
 #PBS -j oe
 
-# on hpc
-module purge
-module load theano/0.7.0
-cd ./NLUDR/NMT-EnglishToChinese/
+## on hpc
+#module purge
+#module load theano/0.7.0
+#cd ./NLUDR/NMT-EnglishToChinese/
 
-THEANO_FLAGS=floatX=float32 python -u -m code.model.attention_nmt.simple_translater -n \
-    /home/ql516/NLUDR/NMT-EnglishToChinese/results/attention_nmt_seg/model.npz \
-    /home/ql516/NLUDR/NMT-EnglishToChinese/data/zh/seg/zh.train.seg.tok.pkl \
-    /home/ql516/NLUDR/NMT-EnglishToChinese/data/en/train.en.tok.pkl \
-    /home/ql516/NLUDR/NMT-EnglishToChinese/data/zh/seg/zh.test.seg.tok \
-    /home/ql516/NLUDR/NMT-EnglishToChinese/results/attention_nmt_seg/test.translation 
-
-
-
-## on laptop
-#cd ../..
 #THEANO_FLAGS=floatX=float32 python -u -m code.model.attention_nmt.simple_translater -n \
-    #$HOME/Desktop/cNLP/project/NMT-EnglishToChinese/results/attention_nmt_seg/model.npz \
-    #$HOME/Desktop/cNLP/project/NMT-EnglishToChinese/data/zh/seg/zh.train.seg.tok.pkl \
-    #$HOME/Desktop/cNLP/project/NMT-EnglishToChinese/data/en/train.en.tok.pkl \
-    #$HOME/Desktop/cNLP/project/NMT-EnglishToChinese/data/zh/seg/zh.test.seg.tok \
-    #$HOME/Desktop/cNLP/project/NMT-EnglishToChinese/results/attention_nmt_seg/test.translation
+    #/home/ql516/NLUDR/NMT-EnglishToChinese/results/attention_nmt_seg/model.npz \
+    #/home/ql516/NLUDR/NMT-EnglishToChinese/data/zh/seg/zh.train.seg.tok.pkl \
+    #/home/ql516/NLUDR/NMT-EnglishToChinese/data/en/train.en.tok.pkl \
+    #/home/ql516/NLUDR/NMT-EnglishToChinese/data/zh/seg/zh.test.seg.tok \
+    #/home/ql516/NLUDR/NMT-EnglishToChinese/results/attention_nmt_seg/test.translation 
+
+
+
+# on laptop
+cd ../..
+THEANO_FLAGS=floatX=float32 python -u -m code.model.attention_nmt.simple_translater -n \
+    $HOME/Desktop/cNLP/project/NMT-EnglishToChinese/results/attention_nmt_no_seg/model.npz \
+    $HOME/Desktop/cNLP/project/NMT-EnglishToChinese/data/zh/no_seg/zh.train.no_seg.tok.pkl \
+    $HOME/Desktop/cNLP/project/NMT-EnglishToChinese/data/en/train.en.tok.pkl \
+    $HOME/Desktop/cNLP/project/NMT-EnglishToChinese/data/zh/no_seg/zh.test.no_seg.tok \
+    $HOME/Desktop/cNLP/project/NMT-EnglishToChinese/results/attention_nmt_no_seg/test.translation.no_seg

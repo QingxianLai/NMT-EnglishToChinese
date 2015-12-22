b4fad69ee11a0c7fad989d0f6a6e2311b2a869ab
QingxianLai <ql516@nyu.edu>
2015-12-22 17:18:55 -0500

    add experiment larger dit

---
+++ code/experiments/attention_nmt_seg_larger_dict/run.q
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=1:gpus=1
+#PBS -l walltime=72:00:00
+#PBS -l mem=5GB
+#PBS -N default
+#PBS -j oe
+
+cd ./NLUDR/NMT-EnglishToChinese/
+
+module purge
+
+module load cuda/7.0.28
+module load theano/0.7.0
+module load ipdb/0.8
+
+THEANO_FLAGS=device=gpu,floatX=float32 python -u -m code.experiments.attention_nmt_seg.train_nmt 
---
+++ code/experiments/attention_nmt_seg_larger_dict/train_nmt.py
@@ -0,0 +1,47 @@
+import numpy
+import os
+
+from code.model.attention_nmt.nmt import train
+
+def main(job_id, params):
+    print params
+    validerr = train(saveto=params['model'][0],
+                                        reload_=params['reload'][0],
+                                        dim_word=params['dim_word'][0],
+                                        dim=params['dim'][0],
+                                        n_words=params['n-words'][0],
+                                        n_words_src=params['n-words'][0],
+                                        decay_c=params['decay-c'][0],
+                                        clip_c=params['clip-c'][0],
+                                        lrate=params['learning-rate'][0],
+                                        optimizer=params['optimizer'][0],
+                                        maxlen=50,
+                                        batch_size=32,
+                                        valid_batch_size=32,
+					datasets=['/home/ql516/NLUDR/NMT-EnglishToChinese/data/zh/seg/zh.train.seg.tok',
+					'/home/ql516/NLUDR/NMT-EnglishToChinese/data/en/train.en.tok'],
+					valid_datasets=['/home/ql516/NLUDR/NMT-EnglishToChinese/data/zh/seg/zh.dev.seg.tok',
+				        '/home/ql516/NLUDR/NMT-EnglishToChinese/data/en/dev.en.tok'],
+					dictionaries=['/home/ql516/NLUDR/NMT-EnglishToChinese/data/zh/seg/zh.train.seg.tok.pkl',
+					'/home/ql516/NLUDR/NMT-EnglishToChinese/data/en/train.en.tok.pkl'],
+                                        validFreq=5000,
+                                        dispFreq=10,
+                                        saveFreq=5000,
+                                        sampleFreq=1000,
+                                        use_dropout=params['use-dropout'][0])
+    return validerr
+
+if __name__ == '__main__':
+    main(0, {
+        'model': ['/home/ql516/NLUDR/NMT-EnglishToChinese/code/experiments/attention_nmt_seg/model.npz'],
+        'dim_word': [700],
+        'dim': [1024],
+        'n-words': [80000],
+        'optimizer': ['adadelta'],
+        'decay-c': [0.],
+        'clip-c': [1.],
+        'use-dropout': [False],
+        'learning-rate': [0.0001],
+        'reload': [False]})
+
+
